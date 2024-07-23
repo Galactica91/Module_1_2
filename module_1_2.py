b3868c411d29c92b7f95c73803f672f13532df0e
Galactica91 <osheva91@yandex.com>
2024-07-23 15:43:58 +0300

    Add files via upload

---
+++ module_1_2.py
@@ -0,0 +1,9 @@
+amount_of_homework = 12
+work_hours = 1.5 # 90 minutes
+course = 'Python'
+
+time_per_job = work_hours / amount_of_homework
+print(time_per_job)
+
+print('Курс' ':', course,',', 'всего задач' ':', amount_of_homework,'затрачено часов' ':', work_hours, 'среднее время выполнения' ':', time_per_job )
+
